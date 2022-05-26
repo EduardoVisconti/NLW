700d100a185435077389a2a2b2f87e56ff2465e8
Eduardo Visconti <duduhsg@gmail.com>
2022-05-26 09:34:37 -0700

    aula05

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 window.addEventListener('scroll', onScroll)
 
 function onScroll() {
-  showNavOnScroll();
-  showBackToTopButtonOnScroll();
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection()
+}
+
+function activateMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da seção
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetheight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // info dos dados e da lógica
+  console.log(
+    'O topo da seção chegou ou passou da linha?',
+    sectionTopReachOrPassedTargetLine
+  )
+
+  // verificar se a base está abaixo da linha alvo
+  // quais dados vou precisar?
+
+  // a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da
+  const sectionEndPassedTargetLine = 
 }
 
 function showNavOnScroll() {
   if (scrollY > 0) {
-    navigation.classList.add('scroll');
+    navigation.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll');
+    navigation.classList.remove('scroll')
   }
 }
 
@@ -22,11 +56,11 @@ function showBackToTopButtonOnScroll() {
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded');
+  document.body.classList.add('menu-expanded')
 }
 
 function closeMenu() {
-  document.body.classList.remove('menu-expanded');
+  document.body.classList.remove('menu-expanded')
 }
 
 ScrollReveal({
